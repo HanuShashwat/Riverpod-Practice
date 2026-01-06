5c171caa49159a0bffdb7ffba8e3c3a54937295b
Hanu Shashwat <hanushashwat733@gmail.com>
2026-01-06 23:10:12 +0530

    learnt state provider

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,30 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_practice/main.dart';
 
-class MyWidget extends ConsumerStatefulWidget {
-  const MyWidget({super.key});
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen ({super.key});
 
-  @override
-  ConsumerState<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends ConsumerState<MyWidget> {
-
-  @override
-  @override
-  void initState() {
-    super.initState();
+  void onSubmit(WidgetRef ref, String value) {
+    final name = ref.read(nameProvider.notifier).update((state) => value);
   }
 
   @override
-  Widget build(BuildContext context) {
-    final name = ref.watch(nameProvider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider) ?? " ";
+
     return Scaffold(
       appBar: AppBar(
         title: Text(name),
       ),
       body: Column(
         children: [
+          TextField(
+            onSubmitted: (value) => onSubmit(ref, value),
+          ),
           Center(
             child: Text(name),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:riverpod_practice/home_screen.dart';
 
 // Providers
 // Provider
+// StateProvider
 
-final nameProvider = Provider<String>((ref) => "Hanu Shashwat");
+//final nameProvider = Provider<String>((ref) => "Hanu Shashwat");
+final nameProvider = StateProvider<String?>((ref) => null);
 
 void main() {
   runApp(
@@ -22,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Riverpod Practice',
-      home: MyWidget(),
+      home: HomeScreen(),
     );
   }
 }
